ebd578d85cecdb27709d5654f8898e2fd6e07b26
Diego Fornalha <diegofornalha@gmail.com>
2024-12-13 05:22:41 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+// @ts-check
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+    formats: ["image/avif", "image/webp"],
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+  typescript: {
+    // !! WARN !!
+    // Permite que builds completem mesmo com erros de TypeScript
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
